3830ab6c18553dfafa7991da7dcb2577b0f1288e
BRYAN ASEGA <111067639+Asega-bryan07@users.noreply.github.com>
2024-05-27 19:01:31 +0300

    Update like_blog.js

--- NextGeo/assets/js/like_blog.js
+++ NextGeo/assets/js/like_blog.js
@@ -1,6 +1,5 @@
-const likeIcon = document.getElementById('like-icon');
-const likeCount = document.getElementById('like-count');
-
+const likeIcon = document.getElementById('like-icon') as HTMLElement;
+const likeCount = document.getElementById('like-count') as HTMLElement;
 
 likeIcon.onclick = () => {
     const blogId = likeIcon.getAttribute('data-blog');
@@ -8,22 +7,24 @@ likeIcon.onclick = () => {
     fetch(url, {
         method: 'GET',
         headers: {
-            'Content-type': 'applicatin/json'
+            'Content-type': 'application/json'
         }
     })
     .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
         return response.json();
     })
     .then(data => {
-        if(data.liked) {
+        if (data.liked) {
             likeIcon.classList.remove('empty-heart');
-        }
-        else {
+        } else {
             likeIcon.classList.add('empty-heart');
         }
-        likeCount.innerHTML = data.like_count;
+        likeCount.innerHTML = data.like_count.toString(); // Ensure like_count is a string
     })
     .catch(error => {
-        console.log(error);
-    })
-}
+        console.error('There was a problem with the fetch operation:', error);
+    });
+};
